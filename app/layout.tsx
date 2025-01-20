803499d0cdc0b298b26dda75e3f3a6d5d9674302
lazermagma <masterabdelrahman7@gmail.com>
2025-01-20 21:11:18 +0200

    implement initial structure

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+  title: "AC bank",
+  description: "AC bank is a banking platform created by Abdelrahman Soliman ",
+  icons: {
+    icon: "/icons/logo.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable} `}>
+        {children}
+      </body>
+    </html>
+  );
+}
