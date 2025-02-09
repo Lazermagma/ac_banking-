63a3063e201080dae00fad87085a7655ae8e9c1c
lazermagma <masterabdelrahman7@gmail.com>
2025-02-09 23:32:52 +0200

    ADD: items to side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -11,9 +17,30 @@ const Sidebar = ({ user }: SiderbarProps) => {
             width={34}
             height={34}
             alt="Ac bank logo"
-            
+            className="size-[24] max-xl:size-14"
           />
+          <h1 className="sidebar-logo">AC Bank</h1>
         </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              className={cn("sidebar-link", {
+                "bg-gradient-to-r to-bankRed from-[#c40658]": isActive,
+              })}
+              href={item.route}
+              key={item.label}
+            >
+              <div className="relative size-6">
+                <Image src={item.imgURL} alt={item.label} fill />
+              </div>
+
+              {item.label}
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
